449b39009a87cfce957862c6f68dedb936649a1b
Sean Chou <champagne0906@gmail.com>
2021-11-14 12:05:17 +0800

    Add check array

--- scripts/helper.js
+++ scripts/helper.js
@@ -12,28 +12,35 @@ hexo.extend.filter.register("after_generate", function () {
 });
 
 hexo.extend.helper.register("__", function (link) {
-  var lang = this.page.lang;
-  var target = link.split(".");
-  var find = hexo.locals.get("pages").filter(function (p) {
-    return p.id == target[0] && p.lang == lang;
-  }).data;
+  try {
+    var lang = this.page.lang;
+    var target = link.split(".");
+    var find = hexo.locals.get("pages").filter(function (p) {
+      return p.id == target[0] && p.lang == lang;
+    }).data;
 
-  if (find.length == 0) return null;
+    if (find.length == 0) return null;
 
-  var result = find[0];
-  target.shift();
-  target.forEach((i) => {
-    result = result[i];
-  });
-  if (result == undefined) return null;
+    var result = find[0];
+    target.shift();
+    target.forEach((i) => {
+      result = result[i];
+    });
+    if (result == undefined) return null;
 
-  return result;
+    return result;
+  } catch (e) {
+    return null;
+  }
+});
+
+hexo.extend.helper.register("array", function (array) {
+  if (Array.isArray(array)) return array;
+  else return [];
 });
 
 const path = require("path");
 hexo.extend.filter.register("before_post_render", function (data) {
-  
-
   let html_path = data.path.split("/");
 
   // Set lang
@@ -64,7 +71,7 @@ hexo.extend.filter.register("before_post_render", function (data) {
 
   data.path = html_path.join("/") + "/";
   console.log(data.path);
-  
+
   return data;
 });
 
